4a5312ebef61c16eee10c061884a92bacbdea100
pablo.silva <pablo.silva@doclerholding.com>
2018-10-17 11:36:56 +0200

    moved template

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const path = require('path')
 const fse = require('fs-extra')
 
-module.exports = name => {
-    const source = path.resolve(__dirname, name)
-    const destination = path.resolve(process.cwd(), `src`)
+module.exports = (name = 'default', srcPath = 'src') => {
+    console.log('name', name)
+    console.log('srcPath', srcPath)
+    const source = path.resolve(__dirname, `templates/${name}`)
+    const destination = path.resolve(process.cwd(), srcPath)
 
     fse.copy(source, destination, function (err) {
-        if (err){
+        if (err) {
             return console.error('@mhy/boot', 'An error occured while copying the source to the destination.', err)
         }
         console.log('Boot completed!')
